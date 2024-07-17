52cbbcb5e7ef9f28dde01e8024b4e4408663fff0
Krishna Kumar <krishnakumar750514@gmail.com>
2024-07-17 17:49:49 +0530

    all three approaches

--- leetcode/746_min_cost_climbing_stairs.cpp
+++ leetcode/746_min_cost_climbing_stairs.cpp
@@ -22,14 +22,48 @@ public:
         return dp[n];
     }
 
+    int solve2(vector<int>&cost, int n)
+    {
+        vector<int>dp(n+1);
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i=2; i<n; i++)
+        {
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+
+        return min(dp[n-1], dp[n-2]);
+    }
+
+    int solve3(vector<int>&cost, int n)
+    {
+        int prev2 = cost[0];
+        int prev1 = cost[1];
+
+        for(int i=2; i<n; i++)
+        {
+            int curr = cost[i] + min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return min(prev1, prev2);
+    }
+
     int minCostClimbingStairs(vector<int>& cost) 
     {
         int n = cost.size();
 
-        vector<int>dp(n+1, -1);
+        // vector<int>dp(n+1, -1);                                           Top-Down Approach
+        // int ans = min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+        // return ans;
 
-        int ans = min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+        // int ans = solve2(cost, n);                                        Bottom-UP Approach
+        // return ans;
 
+        int ans = solve3(cost, n);                                           // Space Optimization
         return ans;
     }
 };
