4d2bbbd67e9bad391c53d4034440a6aaa576b0d0
Krishna Kumar <krishnakumar750514@gmail.com>
2024-08-10 16:04:28 +0530

    linked list problem

---
+++ geeksforgeeks/potd_rotate_a_linked_list.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    Node* rotate(Node* head, int k) 
+    {
+        Node* temp = head;
+        
+        while(temp->next!=NULL)
+        {
+            temp = temp->next;
+        }
+        
+        temp->next = head;
+        
+        while(--k)
+        {
+            head = head->next;
+        }
+        
+        Node* ans = head->next;
+        head->next = NULL;
+        
+        return ans;
+    }
+};
