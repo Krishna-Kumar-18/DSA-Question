6967f4289c3b6c1c28cbd59a65d594df70ad8c3b
Krishna Kumar <krishnakumar750514@gmail.com>
2024-08-15 14:24:38 +0530

    another approach of this problem

--- basic_DSA_question/longest_common_substring.cpp
+++ basic_DSA_question/longest_common_substring.cpp
@@ -26,3 +26,37 @@ class Solution {
         return res;
     }
 };
+
+                                                                // another approach
+
+
+class Solution {
+  public:
+    int longestCommonSubstr(string str1, string str2) 
+    {
+        int n = str1.length();
+        int m = str2.length();
+        
+        vector<int>prev(m+1, 0), curr(m+1, 0);
+        
+        int ans = 0;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(str1[i-1] == str2[j-1])
+                {
+                    curr[j] = 1 + prev[j-1];
+                    ans = max(curr[j], ans);
+                }
+                else 
+                {
+                    curr[j] = 0;
+                }
+            }
+            prev = curr;
+        }
+        return ans;
+    }
+};
+
