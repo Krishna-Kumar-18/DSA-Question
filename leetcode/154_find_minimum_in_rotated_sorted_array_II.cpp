e4d930ef7c51296f8faefebd4d5b8c70bb3d7e2f
Krishna Kumar <krishnakumar750514@gmail.com>
2024-08-30 11:21:11 +0530

    approach using binary search

--- leetcode/154_find_minimum_in_rotated_sorted_array_II.cpp
+++ leetcode/154_find_minimum_in_rotated_sorted_array_II.cpp
@@ -10,3 +10,41 @@ public:
         return ans;
     }
 };
+
+
+// another approach using Binary Search
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) 
+    {
+        int low = 0;
+        int high = nums.size()-1;
+        int mid = low + (high-low)/2;
+
+        while(low < high)
+        {   if(nums[mid]==nums[high])
+            {
+                if(nums[low]==nums[mid])
+                {
+                    low++;
+                    high--;
+                }
+                else
+                {
+                    high = mid;
+                }
+            } 
+            else if(nums[mid] > nums[high])
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid;
+            }
+            mid = low + (high-low)/2;
+        }
+        return nums[high];
+    }
+};
