7a73c94e1514f80a4ee68a0563bbd98f50435fb8
Krishna Kumar <krishnakumar750514@gmail.com>
2024-09-05 20:13:55 +0530

    array problem

---
+++ geeksforgeeks/potd_missing_in_array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int missingNumber(int n, vector<int>& arr) 
+    {
+        sort(arr.begin(), arr.end());
+        
+        for(int i=0; i<arr.size(); i++)
+        {
+            if(arr[i]!=i+1)
+            {
+                return i+1;
+            }
+        }
+        return n;
+    }
+};
