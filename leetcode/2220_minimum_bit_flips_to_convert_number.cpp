a6838215d18c39388ed72cb8ee8051f492fd9194
Krishna Kumar <krishnakumar750514@gmail.com>
2024-09-11 15:05:20 +0530

    based on bit manipulation

---
+++ leetcode/2220_minimum_bit_flips_to_convert_number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) 
+    {
+        int num = start^goal;
+
+        int ans=0;
+        for(int i=0; i<32; i++)
+        {
+            if(num&1)
+            {
+                ans++;
+            }
+            num = num >> 1;
+        }
+    
+        return ans;
+    }
+};
