9f91de929b2c574d07bb4b900dbb9580dfaeff5b
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2024-10-03 13:12:25 +0530

    moore algorithm

---
+++ algorithm/potd_majority_vote.cpp
@@ -0,0 +1,71 @@
+class Solution {
+  public:
+    vector<int> findMajority(vector<int>& nums) 
+    {
+        vector<int>ans;
+        
+        int ele_1=nums[0], ele_2=nums[0];
+        int count_1=0, count_2=0;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(ele_1==nums[i])
+            {
+                count_1++;
+            }
+            else if(ele_2==nums[i])
+            {
+                count_2++;
+            }
+            else if(count_1==0)
+            {
+                ele_1 = nums[i];
+                count_1 = 1;
+            }
+            else if(count_2==0)
+            {
+                ele_2 = nums[i];
+                count_2 = 1;
+            }
+            else 
+            {
+                count_1--;
+                count_2--;
+            }
+        }
+        
+        int freq_1=0, freq_2=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(ele_1==nums[i])
+            {
+                freq_1++;
+            }
+            else if(ele_2==nums[i])
+            {
+                freq_2++;
+            }
+        }
+        
+        int vote = nums.size()/3;
+        
+        if(freq_1>vote)
+        {
+            ans.push_back(ele_1);
+        }
+        
+        if(freq_2>vote)
+        {
+            ans.push_back(ele_2);
+        }
+        
+        sort(ans.begin(), ans.end());
+        
+        if(ans.size()==0)
+        {
+            ans.push_back(-1);
+        }
+        
+        return ans;
+    }
+};
