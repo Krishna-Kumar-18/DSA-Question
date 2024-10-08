dfdb914f05a7845dfc390bf4c783aead9fd69caf
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2024-10-08 14:48:00 +0530

    based on stack

---
+++ leetcode/2696.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minLength(string s) 
+    {
+        stack<int>st;
+
+        for(int i=0; i<s.length(); i++)
+        {
+            if(st.empty())
+            {
+                st.push(s[i]);
+            }
+            else if(st.top()=='A' && s[i]=='B')
+            {
+                st.pop();
+            }
+            else if(st.top()=='C' && s[i]=='D')
+            {
+                st.pop();
+            }
+            else 
+            {
+                st.push(s[i]);
+            }
+        }
+
+        return st.size();
+    }
+};
