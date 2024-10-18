4047db61a766c6d386c98b445c3c5f8d795015f1
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2024-10-18 22:01:20 +0530

    docs(greddy): greddy problem - based on two pointer approach

---
+++ leetcode/2938_separate_black_and_white_balls.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    long long minimumSteps(string s) 
+    {
+        int n = s.length();
+
+        vector<int>v;
+
+        long long ind = -1;
+        for(long long i=n-1; i>=0; i--)
+        {
+            if(s[i]!='1')
+            {
+                ind = i;
+                break;
+            }
+        }
+
+        if(ind==-1)
+        {
+            return 0;
+        }
+
+        for(long long i=ind; i>=0; i--)
+        {
+            if(s[i]=='1')
+            {
+                v.push_back(i);
+            }
+        }
+
+        if(v.size()==0)
+        {
+            return 0;
+        }
+
+        long long ans = 0;
+
+        for(long long i : v)
+        {
+            ans += (ind-i);
+            ind--;
+        }
+
+        return ans;
+    }
+};
