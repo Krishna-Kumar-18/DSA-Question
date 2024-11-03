6214a60288dab0a329197c34c3afd75646c582a0
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2024-11-03 22:13:55 +0530

    Dp Problem - Bottom Up Approach

--- dp/leetcode_198_house_robber.cpp
+++ dp/leetcode_198_house_robber.cpp
@@ -44,43 +44,34 @@ public:
 
 class Solution {
 public:
-    int solve(vector<int>& nums, int ind, vector<int>&dp)
-    {
-        if(ind==0)
-        {
-            return nums[ind];
-        }
-
-        if(ind<0)
-        {
-            return 0;
-        }
-
-        if(dp[ind]!=-1)
-        {
-            return dp[ind];
-        }
-
-        int pick = nums[ind] + solve(nums, ind-2, dp);
-        int non_pick = 0 + solve(nums, ind-1, dp);
-
-        dp[ind] = max(pick, non_pick);
-
-        return dp[ind];
-    }
-
     int rob(vector<int>& nums) 
     {
         int n = nums.size();
 
-        vector<int>dp(n+1, -1);
+        vector<int>dp(n, 0);
 
-        int ans = solve(nums, n-1, dp);
+        dp[0] = nums[0];
 
-        return ans;
+        int pick = INT_MIN;
+        for(int i=1; i<n; i++)
+        {
+            if(i-2<0)
+            {
+                pick = nums[i] + 0;
+            }
+            else
+            {
+                pick = nums[i] + dp[i-2];
+            }
+
+            int non_pick = 0 + dp[i-1];
+ 
+            dp[i] = max(pick, non_pick);
+        }
+
+        return dp[n-1];
     }
 };
-
 
 
 
