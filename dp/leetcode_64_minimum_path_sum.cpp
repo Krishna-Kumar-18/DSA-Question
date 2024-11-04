c015bc7d09e0222e1640cd0556e9f0875a6e636d
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2024-11-05 00:17:49 +0530

    DP problem - tabulation

--- dp/leetcode_64_minimum_path_sum.cpp
+++ dp/leetcode_64_minimum_path_sum.cpp
@@ -39,3 +39,55 @@ public:
         return ans;
     }
 };
+
+
+
+
+                                                                            // Tabulation
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) 
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>>dp(n, vector<int>(m, 0));
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(i==0 && j==0)
+                {
+                    dp[i][j] = grid[i][j];
+                }
+                else 
+                {
+                    int down = grid[i][j];
+                    if(i>0)
+                    {
+                        down += dp[i-1][j];
+                    }
+                    else 
+                    {
+                        down += 1e9+7;
+                    }
+
+                    int right = grid[i][j];
+                    if(j>0)
+                    {
+                        right += dp[i][j-1];
+                    }
+                    else 
+                    {
+                        right += 1e9+7;
+                    }
+                    dp[i][j] = min(down, right);
+                }
+            }
+        }
+
+        return dp[n-1][m-1];
+    }
+};
