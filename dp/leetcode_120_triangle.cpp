312c6d3c8cdf19ce594f467bc5ac48311f4cb7ae
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2024-11-06 16:29:56 +0530

    DP problem - top down approach

---
+++ dp/leetcode_120_triangle.cpp
@@ -0,0 +1,36 @@
+                                                                                    // Top Down Approach (Recursion that's why top-down)
+                                                                                    
+class Solution {
+public:
+    int solve(vector<vector<int>>&triangle, int i, int j, vector<vector<int>>&dp)
+    {
+        if(i==triangle.size()-1)
+        {
+            return triangle[i][j];
+        }
+
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+
+        int down = triangle[i][j] + solve(triangle, i+1, j, dp);
+        int diagonal = triangle[i][j] + solve(triangle, i+1, j+1, dp);
+
+        dp[i][j] = min(down, diagonal);
+
+        return dp[i][j];
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) 
+    {
+        int n = triangle.size();
+        int m = triangle[n-1].size();
+
+        vector<vector<int>>dp(n, vector<int>(m, -1));
+        
+        int ans = solve(triangle, 0, 0, dp);
+
+        return ans;
+    }
+};
