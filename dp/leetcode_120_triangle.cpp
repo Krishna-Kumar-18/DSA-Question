df34de3ffa3ca811ce509c1f45ac138107df165f
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2024-11-06 16:47:42 +0530

    DP problem - space optimization

--- dp/leetcode_120_triangle.cpp
+++ dp/leetcode_120_triangle.cpp
@@ -68,3 +68,40 @@ public:
         return dp[0][0];
     }
 };                                                             
+
+
+
+
+
+                                                                                // Space Optimization
+
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) 
+    {
+        int n = triangle.size();
+
+        vector<int>prev(n, 0);
+        
+        for(int j=0; j<n; j++)
+        {
+            prev[j] = triangle[n-1][j];
+        }
+
+        for(int i=n-2; i>=0; i--)
+        {
+            vector<int>curr(n, 0);
+            for(int j=i; j>=0; j--)
+            {
+                int down = triangle[i][j] + prev[j];
+                int diagonal = triangle[i][j] + prev[j+1];
+
+                curr[j] = min(down, diagonal);
+            }
+            prev = curr;
+        }
+
+        return prev[0];
+    }
+};                                                                            
