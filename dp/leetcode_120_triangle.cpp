0657e4d2c723490e702a7352aec1ed0fb710077a
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2024-11-06 16:37:58 +0530

    DP problem - tabulation

--- dp/leetcode_120_triangle.cpp
+++ dp/leetcode_120_triangle.cpp
@@ -1,5 +1,5 @@
                                                                                     // Top Down Approach (Recursion that's why top-down)
-                                                                                    
+
 class Solution {
 public:
     int solve(vector<vector<int>>&triangle, int i, int j, vector<vector<int>>&dp)
@@ -34,3 +34,37 @@ public:
         return ans;
     }
 };
+
+
+
+
+                                                                                // Tabulation
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) 
+    {
+        int n = triangle.size();
+        int m = triangle[n-1].size();
+
+        vector<vector<int>>dp(n, vector<int>(m, 0));
+        
+        for(int j=0; j<n; j++)
+        {
+            dp[n-1][j] = triangle[n-1][j];
+        }
+
+        for(int i=n-2; i>=0; i--)
+        {
+            for(int j=i; j>=0; j--)
+            {
+                int down = triangle[i][j] + dp[i+1][j];
+                int diagonal = triangle[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min(down, diagonal);
+            }
+        }
+
+        return dp[0][0];
+    }
+};                                                             
