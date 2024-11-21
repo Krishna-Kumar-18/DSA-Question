1f48c76ce1e8bc27f6767760bc1f1ab17b7a02c3
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2024-11-21 18:15:44 +0530

    dp problem

---
+++ dp/gfg_stock_buy_and_sell_multiple_transaction_allowed.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  public:
+    int solve(vector<int>&prices, int buy, int ind, vector<vector<int>>&dp)
+    {
+        if(ind==prices.size())
+        {
+            return 0;
+        }
+        
+        if(dp[ind][buy] != -1)
+        {
+            return dp[ind][buy];
+        }
+        
+        int profit = 0;
+        
+        if(buy)
+        {
+            profit = max(-prices[ind] + solve(prices, 0, ind+1, dp), 0 + solve(prices, 1, ind+1, dp));
+        }
+        else 
+        {
+            profit = max(prices[ind] + solve(prices, 1, ind+1, dp), 0 + solve(prices, 0, ind+1, dp));
+        }
+        
+        dp[ind][buy] = profit;
+        
+        return dp[ind][buy];
+    }
+    
+    int maximumProfit(vector<int> &prices) 
+    {
+        int n = prices.size();
+        
+        vector<vector<int>>dp(n, vector<int>(2, -1));
+        
+        int ans = solve(prices, 1, 0, dp);
+        
+        return ans;
+    }
+};
