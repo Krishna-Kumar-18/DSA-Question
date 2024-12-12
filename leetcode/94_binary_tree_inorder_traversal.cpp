02050713dfccd142a6845657d56266d378e13c20
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2024-12-12 19:52:56 +0530

    solve using iterative approach

--- leetcode/94_binary_tree_inorder_traversal.cpp
+++ leetcode/94_binary_tree_inorder_traversal.cpp
@@ -1,3 +1,5 @@
+                                                                // Using Recursion
+
 class Solution {
 public:
     void inOrder(TreeNode* root, vector<int>&ans)
@@ -17,5 +19,66 @@ public:
         inOrder(root, ans);
 
         return ans;
+    }
+};
+
+
+
+
+
+
+
+                                                                // Iterative Approach or Done is using stack
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+        vector<int>inOrder;
+
+        stack<TreeNode*>st;
+
+        if(root == NULL)
+        {
+            return inOrder;
+        }
+
+        TreeNode* node = root;
+
+        while(true)
+        {
+            if(node != NULL)
+            {
+                st.push(node);
+                node = node->left;
+            }
+            else
+            {
+                if(st.empty())
+                {
+                    break;
+                }
+
+                node = st.top();
+                st.pop();
+                
+                inOrder.push_back(node->val);
+                node = node->right;
+            }
+        }
+
+        return inOrder;
     }
 };
