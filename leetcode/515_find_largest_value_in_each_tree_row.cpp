43b246cb061d76fbd2bb3fd95aeaeebd508af1d0
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2024-12-25 09:52:43 +0530

    solved using level order traversal

---
+++ leetcode/515_find_largest_value_in_each_tree_row.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) 
+    {
+        queue<TreeNode*>q;
+        vector<int>ans;
+
+        if(root==NULL)
+        {
+            return ans;
+        }
+
+        int maxi = INT_MIN;
+
+        q.push(root);
+        q.push(NULL);
+
+        while(!q.empty())
+        {
+            TreeNode* temp = q.front();
+            q.pop();
+
+            if(temp == NULL)
+            {
+                ans.push_back(maxi);
+                maxi = INT_MIN;
+                if(!q.empty())
+                {
+                    q.push(NULL);
+                }
+            }
+            else 
+            {
+                maxi = max(maxi, temp->val);
+                if(temp->left)
+                {
+                    q.push(temp->left);
+                }
+
+                if(temp->right)
+                {
+                    q.push(temp->right);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
