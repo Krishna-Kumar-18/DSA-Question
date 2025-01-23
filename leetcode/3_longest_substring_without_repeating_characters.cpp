60289ccff4125c6b6205aaedd419f27725f7c3d2
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2025-01-23 22:16:40 +0530

    based on sliding window

---
+++ leetcode/3_longest_substring_without_repeating_characters.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        int n = s.length();
+
+        map<char, int>mp;
+
+        int len = 0;
+        int max_len = 0;
+        
+        int i=0, j=0;
+
+        while(j<n)
+        {
+            if(mp[s[j]]==0)
+            {
+                len++;
+                mp[s[j]]++;
+            }
+            else
+            {
+                while(mp[s[j]]!=0)
+                {
+                    len--;
+                    mp[s[i]]--;
+                    i++;
+                }
+                len++;
+                mp[s[j]]++;
+            }
+            max_len = max(len, max_len);
+            j++;
+        }
+
+        return max_len;
+    }
+};
