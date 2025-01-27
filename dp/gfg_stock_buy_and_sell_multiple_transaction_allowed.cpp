92daddc55b77ee3660a5884227a1c04a86e48296
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2025-01-27 15:31:23 +0530

    solved without dp

--- dp/gfg_stock_buy_and_sell_multiple_transaction_allowed.cpp
+++ dp/gfg_stock_buy_and_sell_multiple_transaction_allowed.cpp
@@ -39,3 +39,26 @@ class Solution {
         return ans;
     }
 };
+
+
+
+
+                                                                // Another Approach
+
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) 
+    {
+        int profit = 0;
+        
+        for(int i=0; i<prices.size()-1; i++)
+        {
+            if(prices[i+1]>prices[i])
+            {
+                profit += (prices[i+1]-prices[i]);
+            }
+        }
+        
+        return profit;
+    }
+};
