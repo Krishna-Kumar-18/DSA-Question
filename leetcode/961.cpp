fd185cf3f269968760f6f438670576e121b4ef5a
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2026-01-02 22:20:06 +0530

    hash map problem

---
+++ leetcode/961.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) 
+    {
+        int n = nums.size(); 
+
+        unordered_map<int, int>mp;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            mp[nums[i]]++;
+        }
+
+
+        for(auto i : mp)
+        {
+            if(i.second == n/2)
+            {
+                return i.first;
+            }
+        }
+
+        return 1;
+    }
+};
