fd7a260342d7d3eabd517222a8f8ec41c0a44b02
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2026-01-16 23:49:02 +0530

    Stack + Recursion Approach

--- leetcode/112_path_sum.cpp
+++ leetcode/112_path_sum.cpp
@@ -1,3 +1,5 @@
+                                            // Recursion Approach
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -48,6 +50,63 @@ public:
         if(flag)
         {
             return true;
+        }
+
+        return false;
+    }
+};
+
+
+
+
+
+                                                //Stack Approach
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) 
+    {
+        if(root==NULL)
+        {
+            return false;
+        }
+
+        stack<pair<TreeNode*, int>>st;
+        st.push({root, root->val});
+
+        while(!st.empty())
+        {
+            auto [node, currSum] = st.top();
+            st.pop();
+
+            if(node->left==NULL && node->right==NULL)
+            {
+                if(currSum == targetSum)
+                {
+                    return true;
+                }
+            }
+
+            if(node->right)
+            {
+                st.push({node->right, currSum + node->right->val});
+            }
+
+            if(node->left)
+            {
+                st.push({node->left, currSum + node->left->val});
+            }
         }
 
         return false;
