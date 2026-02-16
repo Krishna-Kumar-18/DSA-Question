2d54fe6947e57cc3d38dd807a0c55ba456cd1093
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2026-02-16 23:10:57 +0530

    bit manipulation problem

---
+++ leetcode/190_reverse_bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverseBits(int n) 
+    {
+        long long int ans = 0;
+
+        for(int i=0; i<32; i++)
+        {
+            ans = ans << 1;
+            ans = ans | (n&1);
+            n = n >> 1;
+        }
+
+        return ans;
+    }
+};
